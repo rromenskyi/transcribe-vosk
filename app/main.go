bde0933b2810a468905d4069a9b834ea2d9e5c7e
Roman Romenskyi <roman220220@gmail.com>
2023-12-17 21:32:49 +0100

    init commit

---
+++ app/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+_	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"strconv"
+
+	"github.com/gorilla/websocket"
+)
+
+type VoskResponse struct {
+	Text string `json:"text"`
+}
+
+func main() {
+	// Проверка аргументов командной строки
+	if len(os.Args) < 5 {
+		log.Fatal("Usage: go run main.go <path_to_audio_file> <sample_rate> <hostname> <port>")
+	}
+
+	inputFile := os.Args[1]
+
+	sampleRateStr := os.Args[2]
+	sampleRate, err := strconv.Atoi(sampleRateStr)
+	if err != nil {
+		log.Fatal("Invalid sample rate:", err)
+	}
+
+	hostname := os.Args[3]
+	port := os.Args[4]
+	serverURL := fmt.Sprintf("ws://%s:%s", hostname, port)
+
+	// Преобразование аудио с помощью FFmpeg
+	cmd := exec.Command("ffmpeg", "-i", inputFile, "-ar", fmt.Sprintf("%d", sampleRate), "-ac", "1", "-f", "s16le", "-")
+	audioData, err := cmd.Output()
+	if err != nil {
+		log.Fatal("Failed to process audio:", err)
+	}
+
+	conn, _, err := websocket.DefaultDialer.Dial(serverURL, nil)
+	if err != nil {
+		log.Fatal("Failed to connect to Vosk server:", err)
+	}
+	defer conn.Close()
+
+	// Отправка аудио на сервер Vosk
+	err = conn.WriteMessage(websocket.BinaryMessage, audioData)
+	if err != nil {
+		log.Fatal("Failed to send audio data:", err)
+	}
+
+	// Получение результата распознавания
+	_, message, err := conn.ReadMessage()
+	if err != nil {
+		log.Fatal("Failed to receive response:", err)
+	}
+
+	var response VoskResponse
+	err = json.Unmarshal(message, &response)
+	if err != nil {
+		log.Fatal("Failed to parse response:", err)
+	}
+
+	fmt.Println("Transcription:", response.Text)
+}
+
